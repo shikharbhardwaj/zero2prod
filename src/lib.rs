0e69f08814701b31dd2f535ef5ca075df2d709af
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2023-04-23 15:30:24 +0000

    Add utoipa for openapi spec + docgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,40 @@
 use std::net::TcpListener;
 
-use actix_web::{web, App, HttpServer, Responder, HttpResponse, dev::Server};
+use actix_web::{get, HttpServer, Responder, HttpResponse, dev::Server, App};
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
 
+#[utoipa::path(
+    responses(
+        (status = 200, description = "Application healthy"),
+    ),
+    tag = "zero2prod"
+)]
+#[get("/health_check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error>{
-    let server = HttpServer::new(|| {
+
+    #[derive(OpenApi)]
+    #[openapi(
+        paths(
+            health_check
+        ),
+        tags(
+            (name = "zero2prod", description = "Newsletter app built following the Rust: Zero to Production book.")
+        )
+    )]
+    struct ApiDoc;
+
+    let openapi = ApiDoc::openapi();
+
+    let server = HttpServer::new(move || {
         App::new()
-            .route("/health_check", web::get().to(health_check))
+            .service(health_check)
+            .service(SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-docs/openapi.json", openapi.clone()))
+
     })
     .listen(listener)?
     .run();
