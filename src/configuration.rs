96bdbf869fb39d283df6f180c261a736bc2624d0
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2023-05-06 02:43:42 +0530

    Add trace DB logs

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,6 @@
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::postgres::PgConnectOptions;
+use sqlx::{postgres::PgConnectOptions, ConnectOptions};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -20,7 +20,9 @@ pub struct DatabaseSettings {
 
 impl DatabaseSettings {
     pub fn with_db(&self) -> PgConnectOptions {
-        self.without_db().database(&self.database_name)
+        let mut options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing_log::log::LevelFilter::Trace);
+        options
     }
 
     pub fn without_db(&self) -> PgConnectOptions {
