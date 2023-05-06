a414b84be0c8e70d617fb5c4451a81c6dab829dd
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2023-05-06 16:33:17 +0530

    Add pre-commit script

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+	against=HEAD
+else
+	# Initial commit: diff against an empty tree object
+	against=$(git hash-object -t tree /dev/null)
+fi
+
+# Redirect output to stderr.
+exec 1>&2
+
+if ! cargo fmt -- --check
+then
+    echo "There are some code style issues."
+    echo "Run cargo fmt first."
+    exit 1
+fi
+
+if ! cargo clippy --all-targets -- -D warnings
+then
+    echo "There are some clippy issues."
+    exit 1
+fi
+
+# If there are whitespace errors, print the offending file names and fail.
+exec git diff-index --check --cached "$against" --
